b2bccf03bbdb116915ca98c7fd65e8981ecb1f83
washin <shin321654987@gmail.com>
2018-06-29 12:45:33 -0500

    add Elbow

--- algoritm_kmeans.py
+++ algoritm_kmeans.py
@@ -8,9 +8,9 @@
 import numpy as np
 import random
 import math
+import matplotlib.pyplot as plt
 
-
-# si se le pasa un vector con las los puntos
+# si se le pasa un vector con los puntos
 # retorna un vecntor con las distancias
 # calcula la distancia entre 2 puntos
 def calcular_distancia(punto1, punto2):
@@ -26,7 +26,7 @@ def get_indice_d_centrd_mas_cerca(punto, centroides):
     return distancias.index(min(distancias)) 
 
 
-# retorna los una lista de clusters
+# retorna los una lista de clusters o grupos
 def get_clusters(datos, indices_d_grupos):
     # clusters = np.array()
     k = max(indices_d_grupos) + 1
@@ -149,21 +149,24 @@ def cost_function(x, c, u):
     return sumatoria/m
 
 
-
-# 
+# para un conjunto de datos data, y k grupos
+# retorna los cetroides calculados
+# un array con los grupos creados
+# y el valor del costo para k grupos
 def kmeans(data,k):
     m = len(data)
     g_old_cetroides = get_rand_centroids(data, k)
     g_new_centroides = get_rand_centroids(data, k)
-    g_grupos = calcular_grupos(data, g_old_cetroides)
-
+    g_grupos = None
+    valor_de_costo = 0
     while variacion_de_centroide(g_old_cetroides, g_new_centroides):
         g_old_cetroides = g_new_centroides
-        g_new_centroides = calcular_centroides(data, g_grupos)
         g_grupos = calcular_grupos(data, g_new_centroides)
-
-    return g_new_centroides, get_clusters(data, g_grupos)
+        g_new_centroides = calcular_centroides(data, g_grupos)
+        valor_de_costo = cost_function(data, g_grupos, g_new_centroides)
+    return g_new_centroides, get_clusters(data, g_grupos), valor_de_costo
     
+
 
 def main():
     g_datos = read_data("xclara.csv")
@@ -171,6 +174,11 @@ def main():
     k = 3
     g_old_cetroides = get_rand_centroids(g_datos, k)
     g_new_centroides = get_rand_centroids(g_datos, k)
+    print("Centroides iniciales aleatorios")
+    for i in g_new_centroides:
+        print(i)
+        
+    valor_de_f_cost = 0
     g_indx_grupos = None
 
     # mientras el centroide varie se sigue calculando
@@ -182,17 +190,95 @@ def main():
         g_indx_grupos = calcular_grupos(g_datos, g_new_centroides)
         g_new_centroides = calcular_centroides(g_datos, g_indx_grupos)
         # funcion costo J(c,u)
-        res_cost = cost_function(g_datos, g_indx_grupos, g_new_centroides)
-        print("funcion costo",res_cost)
+        valor_de_f_cost = cost_function(g_datos, g_indx_grupos, g_new_centroides)
+        print("funcion costo para esta iteracion --> ",valor_de_f_cost)
 
-    print("centroides al final de las iteraciones",g_new_centroides)
+    print("Centroides al final de las iteraciones")
+    for i in g_new_centroides:
+        print(i)
+
+    print("costo final --> ",valor_de_f_cost)
+    # no lo mostramos porque son demasiados datos en cada grupo
     # clusters = get_clusters(g_datos, g_indx_grupos)    
     # for i in clusters:
     #     print(list(i))
 
 
+
+def pendiente(p1,p2):
+    x,y = p1[0] - p2[0], p1[1] - p2[1]
+    return y/x
+
+def main_other():
+    g_datos = read_data("xclara.csv")
+    k = 3
+    # centroides, clusters, costo = kmeans(g_datos,k)
+    x = []
+    y = []
+    d = []
+    numero_de_pruebas_k = 10
+    for i in range(numero_de_pruebas_k):
+        k = i+1
+        result = kmeans(g_datos, k)
+        x.append(k)
+        y.append(result[2])
+        d.append([k,result[2]])
+        print("para un K = %d el costo es -> %f"%(k, result[2]))
+
+
+    pendientes = [[pendiente(d[i],d[i+1]),d[i][0]] for i in range(len(d)-1)]
+    # para cual de las pendienes es mas la diferencia
+    # diffs = [[pendientes[i][0] - pendientes[i+1][0], pendientes[i+1][1]] for i in range(len(pendientes)-1)]
+    diffs = [math.fabs(pendientes[i][0] - pendientes[i+1][0]) for i in range(len(pendientes)-1)]
+    ind_m = diffs.index(max(diffs))
+    print(pendientes)
+    print(diffs)
+    # print(ind_m+1)
+    # print(d[ind_m+1][0])
+    print("el optimo k es -> ",x[ind_m+1])
+    # en cual de los ... se dobla mas el ...
+    # ver pendientes para cada 2 puntos y en cual de las pendientes varia mas
+    
+    plt.plot(x,y)
+    plt.grid()
+    plt.show()
+
+    # print(centroides)
+    # print(costo)
+
+
+
+# public static double angleBetweenTwoPointsWithFixedPoint(
+#         double p_1X, double p_1Y, 
+#         double p_2X, double p_2Y, 
+#         double fix_X, double fix_Y)
+
+#     double angle1 = Math.atan2(p_1Y - fix_Y, p_1X - fix_X);
+#     double angle2 = Math.atan2(p_2Y - fix_Y, p_2X - fix_X);
+#     return angle1 - angle2; 
+
+
+def angulo_entre_3_puntos(p1,p2,fix):
+    angulo1 = math.atan2(p1[1] - fix[1], p1[0] - fix[0])
+    angulo2 = math.atan2(p2[1] - fix[1], p2[0] - fix[0])
+    return math.degrees(angulo1 - angulo2)
+
+
+
+def elbow(data):
+    def pendiente(p1,p2):
+        x,y = p1[0] - p2[0], p1[1] - p2[1]
+        return y/x
+    
+
+# EL PROGRAMA MOSTRARA LOS CENTROIDES Y LOS K GRUPOS PARA UN K
+
+def get_k_for_data(data):
+    return None
+
 if __name__ == "__main__":
-    main()
+    # main()
+    main_other()
 
 
 
